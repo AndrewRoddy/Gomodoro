13ee68ee9ce311a1b053440105e44f6e7bfd4ed0
Andrew Roddy <andrewroddy7@gmail.com>
2025-10-11 16:42:58 -0400

    Fixed failed merge

--- src/app/mode-button/mode-button.ts
+++ src/app/mode-button/mode-button.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, effect } from '@angular/core';
+import { TimerService } from '../timer-service';
 
 @Component({
   selector: 'app-mode-button',
-  imports: [],
+  standalone: true,
   templateUrl: './mode-button.html',
   styleUrl: './mode-button.scss'
 })
 export class ModeButton {
 
+  // Inject shared TimerService
+  private timerService = inject(TimerService);
+
+  // Local signal to track current break status
+  public cBreak = signal(this.timerService.isBreak());
+
+  constructor() {
+    // Keep local signal in sync with the TimerService
+    effect(() => {
+      this.cBreak.set(this.timerService.isBreak());
+    });
+  }
+
+  toggleMode() {
+    const newValue = !this.cBreak();
+    this.cBreak.set(newValue);
+    this.timerService.updateBreak(newValue); // Tell the service too
+  }
 }
