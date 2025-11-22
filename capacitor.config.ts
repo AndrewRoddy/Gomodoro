c732f545ec52e1b1361378e23afc5341d72e046d
Andrew Roddy <andrewroddy7@gmail.com>
2025-11-21 21:10:04 -0500

    Compiled with capacitor and ionic!

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gomodoro.app',
+  appName: 'gomodoro',
+  webDir: 'dist/gomodoro/browser'
+};
+
+export default config;
